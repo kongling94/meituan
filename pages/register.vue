a7023325f639f5b84510599a0e1f1c1e05f12732
k.cohen <k.cohen@foxmail.com>
2018-12-07 00:34:54 +0800

    “登录验证”

--- pages/register.vue
+++ pages/register.vue
@@ -17,37 +17,64 @@
     <section>
       <h4 class="title">账号注册</h4>
       <el-form ref="form"
-               :model="form">
-        <el-form-item>
+               :model="form"
+               :rules="rules">
+        <!--  <el-form-item prop="phone">
           <el-input v-model="form.phone">
             <template slot="prepend">
               手机号
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.code">
+        <el-form-item prop="code">
+          <el-input v-model="form.code"
+                    maxlength="4">
             <template slot="prepend">
               验证码
             </template>
           </el-input>
-        </el-form-item>
-        <el-form-item label="">
+        </el-form-item> -->
+        <el-form-item prop="name">
           <el-input v-model="form.name">
             <template slot="prepend">
               用户名
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item label="">
-          <el-input v-model="form.password">
+
+        <el-form-item prop="email">
+          <el-input v-model="form.email">
+            <template slot="prepend">
+              邮箱
+            </template>
+          </el-input>
+          <el-button size="mini"
+                     round
+                     @click="sendMsg">发送验证码</el-button>
+          <span class="status">{{statusMsg}}</span>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-input v-model="form.code"
+                    maxlength="4">
+            <template slot="prepend">
+              验证码
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input v-model="form.password"
+                    type="password">
             <template slot="prepend">
               密码
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item label="">
-          <el-input v-model="form.password">
+        <el-form-item>
+          <el-input v-model="form.password2"
+                    prop="password2"
+                    type="password">
             <template slot="prepend">
               确认密码
             </template>
@@ -58,6 +85,7 @@
           <el-button type="primary"
                      @click="onSubmit">立即创建</el-button>
           <el-button>取消</el-button>
+          <div class="error">{{error}}</div>
         </el-form-item>
       </el-form>
     </section>
@@ -68,17 +96,83 @@ export default {
   layout: 'blank',
   data () {
     return {
+      statusMsg: '',
+      error: '',
       form: {
         name: '',
-        phone: '',
+        email: '',
         code: '',
         password: '',
+        password2: '',
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            type: 'string',
+            message: '用户名不能为空',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            type: 'email',
+            message: '请输入邮箱地址',
+            trigger: 'blur'
+          },
+          {
+            type: 'email',
+            message: '请输入正确的邮箱地址',
+            trigger: ['blur', 'change']
+          }
+        ],
+        code: [
+          {
+            required: true,
+            type: 'number',
+            message: '请输入验证码',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            type: 'string',
+            message: '请输入密码',
+            trigger: 'blur'
+          }
+        ],
+        password2: [
+          {
+            required: true,
+            type: 'string',
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Erroe('密码不能为空'))
+              } else {
+                if (value !== this.form.password) {
+                  callback(new Erroe('两次密码不一样'))
+                }
+              }
+              callback()
+            },
+            trigger: 'blur'
+          }
+        ]
       }
     }
   },
   methods: {
     onSubmit () {
       console.log('submit!');
+    },
+    sendMsg () {
+
     }
   }
 }
