e97656c38958aed54de2a63d5d49b7d186847744
k.cohen <k.cohen@foxmail.com>
2018-12-19 00:34:40 +0800

    “”

---
+++ components/products/categroy.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-product-categroy">
+    <dl class="classic">
+      <dt>分类</dt>
+      <dt>全部</dt>
+      <dd v-for="(item,index) in types"
+          :key="index">
+        <iselect :name="item.type"
+                 :list="item.module"></iselect>
+      </dd>
+    </dl>
+    <dl class="classic">
+      <dt>分类</dt>
+      <dt>全部</dt>
+      <dd v-for="(item,index) in areas"
+          :key="index">
+        <iselect :name="item.type"
+                 :list="item.module"></iselect>
+      </dd>
+    </dl>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+</style>
+
---
+++ components/products/list.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="m-products-list">
+    <dl>
+      <dd v-for="item in nav"
+          :key="item.name"
+          :class="[item.name,item.active?'s-nav-active':'']"
+          @click="navSelect">{{item.txt}}</dd>
+    </dl>
+    <ul>
+      <Item v-for="(item,index) in list"
+            :key="index"
+            :meta="item"></Item>
+    </ul>
+  </div>
+</template>
+<script>
+import Item from './product.vue'
+export default {
+  components: {
+    Item
+  },
+  data () {
+    return {
+      nav: [
+        {
+          name: 's-default',
+          txt: '智能排序',
+          active: true
+        },
+        {
+          name: 's-price',
+          txt: '价格最低',
+          active: false
+        },
+        {
+          name: 's-visit',
+          txt: '人气最高',
+          active: false
+        },
+        {
+          name: 's-comment',
+          txt: '评价最高',
+          active: false
+        }
+
+      ]
+    }
+  },
+  async asyncData ({ app }) { }
+}
+</script>
+<style lang="scss">
+</style>
